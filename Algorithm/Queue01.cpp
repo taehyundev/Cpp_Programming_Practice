741860d67283a81080f9a512b1d4890887704ca0
taehyundev <K.anon.11107@gmail.com>
2020-01-27 01:47:18 +0900

    C:/Program Files/Git/

---
+++ Algorithm/Queue01.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+class Queue {
+private:
+	//Main variables
+	int data[10000];
+	int size;
+	int back;
+	int front;
+public:
+	void push(int n); // Insert
+	int pop(); // Front Remove
+	int frontF(); //Front Infor
+	int backF(); //Back Infor
+	int sizeF(); //size 
+	bool Empty(); //Empty Discrimination
+	Queue();
+
+
+};
+
+//Constructor
+Queue::Queue() :size(0), back(-1), front(-1) {}
+
+//push Func /NOT values = -1
+void Queue::push(int n) {
+	if (front == -1) {
+		front++;
+	}
+	back++;
+	data[back] = n;
+	size++;
+
+}
+
+//pop Func /NOT values = -1
+int Queue::pop() {
+	if (size == 0) {
+		return -1;
+	}
+	return size-- > 0 ? data[front++] : -1;
+}
+
+//front Func
+int Queue::frontF() {
+	return size > 0 ? data[front] : -1;
+}
+
+//back Func
+int Queue::backF() {
+	return size > 0 ? data[back] : -1;
+}
+
+//size F
+int Queue::sizeF() {
+	return size < 0 ? size = 0 : size;
+}
+
+//Empty true = 1 false = 0
+bool Queue::Empty() {
+	return size < 1 ? 0 : 1;
+}
+
+int main() {
+	Queue q;
+	q.push(4);
+	q.push(17);
+	q.push(40);
+	q.push(85);
+	cout << "Push : 4, 17, 40, 85" << endl << endl << "FIFO" << endl;
+	cout << "pop : " << q.pop() << endl;
+	cout << "pop : " << q.pop() << endl;
+	cout << "Front : " << q.frontF() << endl;
+	cout << "Back : " << q.backF() << endl;
+	cout << "Size : " << q.sizeF() << endl;
+	cout << "Empty : " << q.Empty() << endl; // Value's Not Null = 1 Null =0
+	cout << "pop : " << q.pop() << endl;
+	cout << "pop : " << q.pop() << endl;
+	cout << "Empty : " << q.Empty() << endl;
+}
---
+++ Algorithm/linkedlist.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//linked list
+// [DATA | NEXT] >[DATA | NEXT] >[DATA | NEXT] >[DATA | NEXT]
+
+typedef struct _Node {
+	string data;
+	struct _Node* Next;
+}Node;
+
+Node* head, * endd;
+Node* temp1, * temp2, * temp_In;
+int top = 0;
+
+template< class T>
+void init(T n, T n1) {
+
+	head = new Node;
+	head->Next = NULL;
+	endd = new Node;
+	head->Next = endd;
+	endd->Next = NULL;
+
+	temp1 = new Node;
+	head->Next = temp1;
+	temp1->data = n;
+	temp1->Next = endd;
+	temp2 = new Node;
+	temp1->Next = temp2;
+	temp2->Next = endd;
+	temp2->data = n1;
+	top = 2;
+}
+
+void Insert() {
+	string c;
+	cout << "Add Str : ";
+	cin >> c;
+	temp_In = new Node;
+	temp_In->data = c;
+	top++;
+	Node* pt;
+
+	for (pt = head; pt->Next != endd; pt = pt->Next) {
+		if (pt->Next->data > temp_In->data) {
+			break;
+		}
+	}
+
+	temp_In->Next = pt->Next;
+	pt->Next = temp_In;
+
+}
+int deletef() {
+	string  c;
+	cout << "Delete Str : ";
+	cin >> c;
+	Node* ptr;
+	Node* deleteptr;
+	for (ptr = head; ptr->Next != endd; ptr = ptr->Next) {
+		if (c == ptr->Next->data) {
+			deleteptr = ptr->Next;
+			ptr->Next = ptr->Next->Next;
+			delete deleteptr;
+			break;
+		}
+		if (ptr->Next->Next == endd)
+			return -1;
+	}
+
+	top--;
+	return 0;
+}
+
+int main() {
+	int i = 0;
+	Node* ptr;
+	string A, B;
+	cin >> A >> B;
+	string temp;
+	if (A > B) {
+		temp = A;
+		A = B;
+		B = temp;
+	}
+
+	init(A, B);
+
+	ptr = head->Next;
+	for (int i = 0; i < top; i++) {
+		cout << ptr->data << endl;
+		ptr = ptr->Next;
+	}
+
+	Insert();
+
+	ptr = head->Next;
+	for (int i = 0; i < top; i++) {
+		cout << ptr->data << endl;
+		ptr = ptr->Next;
+	}
+
+	if (deletef() == -1) {
+		cout << "삭제하실 정보가 없습니다." << endl;
+	}
+	else {
+
+		cout << "삭제가 완료하였습니다." << endl;
+		ptr = head->Next;
+		for (int i = 0; i < top; i++) {
+			cout << ptr->data << endl;
+			ptr = ptr->Next;
+		}
+	}
+}

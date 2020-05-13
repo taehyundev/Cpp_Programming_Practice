f8b3da43c26913b3919a8f4926b80e4ed47ba64f
taehyundev <K.anon.11107@gmail.com>
2020-05-13 18:55:00 +0900

    과제 추가 7주차

---
+++ CPP 과제/7주차/과제1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void combine(string t1,string t2,string &r) {
+	r=t1+ " "+t2;
+}
+
+int main() {
+	string text1("I love you"), text2("very much");
+	string text3;
+	combine(text1, text2, text3);
+	cout << text3;
+}
